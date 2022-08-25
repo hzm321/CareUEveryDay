3e0e111f1f354ad6b9590aa272eb3daa633b87eb
hzm321 <104822138+hzm321@users.noreply.github.com>
2022-08-25 10:52:42 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx2bcc5abed37049f0";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "deabe63e600d00e53d766d16e79e69fd";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "DNXchSoVJZ1XL19JveeqCg6qjosqmqA71t_AtJHlp4c";
 
     /**
      * 初始化
